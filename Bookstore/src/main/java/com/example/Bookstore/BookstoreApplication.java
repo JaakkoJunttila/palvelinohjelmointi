aa969710818b90b83fcd02cba05fb9dc310f4161
JaakkoJunttila <54263200+JaakkoJunttila@users.noreply.github.com>
2019-09-15 22:05:11 +0300

    Add files via upload

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Bean;
 import com.example.Bookstore.BookstoreApplication;
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
-
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -19,14 +20,18 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner bookstreDemo(BookRepository repository) {
+	public CommandLineRunner bookstreDemo(BookRepository srepository, CategoryRepository drepository) {
 		return (args) -> {
 			log.info("save a couple of books");
-			repository.save(new Book("Steel caves", "Isaac Asimov", 1950, "951-98548-9-4", 120.0));
-			repository.save(new Book("Foundation", "Isaac Asimov", 1975, "551-98548-9-4", 99.95));	
+			drepository.save(new Category("Sci-Fi"));
+			drepository.save(new Category("History"));
+			drepository.save(new Category("Comedy"));
+			
+			srepository.save(new Book("Steel caves", "Isaac Asimov", 1950, "951-98548-9-4", 120.0, drepository.findByName("Sci-Fi").get(0)));
+			srepository.save(new Book("Foundation", "Isaac Asimov", 1975, "551-98548-9-4", 99.95, drepository.findByName("Sci-Fi").get(0)));	
 			
 			log.info("fetch all books");
-			for (Book book : repository.findAll()) {
+			for (Book book : srepository.findAll()) {
 				log.info(book.toString());
 			}
 		};
--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -10,13 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.CategoryRepository;
+
+
 
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
-	@RequestMapping(value= {"/", "/booklist"})
+	@Autowired
+	private CategoryRepository drepository;
+	
+	@RequestMapping(value= {"/", "/booklist", "edit/booklist"})
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";	
@@ -25,20 +31,30 @@ public class BookController {
 	@RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
+    	model.addAttribute("categorys", drepository.findAll());
         return "addbook";
     }
 	
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	
+	@RequestMapping(value = {"/save", "edit/save"}, method = RequestMethod.POST)
     public String save(Book book){
         repository.save(book);
         return "redirect:booklist";
     }
 	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+	@RequestMapping(value = "/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
     	repository.deleteById(bookId);
     	return "redirect:../booklist";
-    }    
+    }
+	
+	@RequestMapping(value = "/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model){
+	model.addAttribute("book", repository.findById(bookId));
+	model.addAttribute("categorys", drepository.findAll());
+	repository.deleteById(bookId);
+	return "addbook";
+	}
          
 }
 
